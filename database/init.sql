7364c86a39d568826ffe1d1bde2954aa0ea3b46a
iAmNsengi <iamnsengi@icloud.com>
2025-02-19 10:22:21 +0200

    fx: Only create table it doesn't exist 🔅

--- database/init.sql
+++ database/init.sql
@@ -1,9 +1,8 @@
 CREATE DATABASE IF NOT EXISTS student_db;
 USE student_db;
 
-DROP TABLE IF EXISTS students;
 
-CREATE TABLE students (
+CREATE TABLE IF NOT EXISTS students (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     age INT NOT NULL,
